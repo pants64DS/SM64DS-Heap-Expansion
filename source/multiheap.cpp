ccf41a717180a7b940ec988d5960ce7d175f03c5
pants64DS <pants64DS@users.noreply.github.com>
2023-12-28 03:53:45 +0200

    Add more unused memory regions

--- source/multiheap.cpp
+++ source/multiheap.cpp
@@ -2,57 +2,145 @@
 #include <new>
 #include <array>
 
-// Set this to true if your hack uses the More Objects Mod, or if you aren't sure if it does.
-// Only set it to false if you're certain that it doesn't.
-#define MOM_OVERLAY_MAY_EXIST true
+#define ITCM_ARENA_START     0x01ffdf40
+#define MAIN_RAM_START       0x02000000
+#define MAIN_RAM_CODE_START  0x02004000
+#define LEVEL_OVERLAY_START  0x0214eaa0
+#define INSERTED_CODE_START  0x02156aa0
+#define DTCM_START           0x023c0000
+#define DTCM_END             0x023c4000
+#define ARM7_ARENA_START     0x023d80e0
+#define FLASHCARD_CODE_START 0x023fc000
 
-struct BaseWithoutMOM
+#define STR(x) #x
+#define REGION(name, start, end) extern char name[(end) - (start)]; \
+asm(#name " = " STR(start));
+
+REGION(UNUSED_START_OF_RAM,      MAIN_RAM_START,      MAIN_RAM_CODE_START)
+REGION(RAM_BEFORE_INSERTED_CODE, LEVEL_OVERLAY_START, INSERTED_CODE_START)
+REGION(UNUSED_END_OF_RAM,        DTCM_END,            FLASHCARD_CODE_START)
+
+struct MemoryRange
 {
-	static constexpr char* extraHeapStorage = UNUSED_RAM_REGION;
+	char* start;
+	char* end;
+
+	template<unsigned size>
+	constexpr MemoryRange(char(&arr)[size]):
+		start(arr),
+		end(arr + size)
+	{}
+
+	constexpr bool Contains(const void* ptr) const
+	{
+		static constexpr std::less<const void*> less = {};
+
+		return !less(ptr, start) && less(ptr, end);
+	}
+
+	constexpr ExpandingHeap& GetHeap() const
+	{
+		return *std::launder(reinterpret_cast<ExpandingHeap*>(start));
+	}
+
+	void ConstuctHeap() const
+	{
+		char* const heapStart = start + sizeof(ExpandingHeap);
+		const unsigned heapSize = end - heapStart;
+
+		auto* allocator = Heap::CreateExpandingHeapAllocator(heapStart, heapSize, 3);
+		if (!allocator) Crash();
+
+		new (start) ExpandingHeap(heapStart, heapSize, nullptr, allocator);
+	}
 };
 
-struct BaseWithMOM
+struct ExtraHeapIterator
 {
-	static inline char* extraHeapStorage = UNUSED_RAM_REGION;
+	const MemoryRange* memRange;
 
-	BaseWithMOM()
+	constexpr bool operator==(const ExtraHeapIterator& other) const = default;
+
+	constexpr ExpandingHeap& operator*() const
+	{
+		return memRange->GetHeap();
+	}
+
+	constexpr ExtraHeapIterator& operator++()
+	{
+		++memRange;
+
+		return *this;
+	}
+};
+
+struct ExtraHeaps
+{
+	std::array<MemoryRange, 3> memRanges
+	{
+		UNUSED_START_OF_RAM,
+		RAM_BEFORE_INSERTED_CODE,
+		UNUSED_END_OF_RAM
+	};
+
+	void Init()
 	{
 		InitFileSystem();
 
 		OverlayInfo ovInfo;
+		unsigned maxLevelOverlaySize = 0;
+
+		for (unsigned i = 0; i < 52; ++i)
+		{
+			unsigned ovID = i + 103;
+
+			if (!LoadOverlayInfo(ovInfo, false, ovID))
+				Crash();
+
+			const unsigned overlaySize = ovInfo.loadSize + ovInfo.bssSize;
+
+			if (maxLevelOverlaySize < overlaySize)
+				maxLevelOverlaySize = overlaySize;
+		}
+
+		memRanges[1].start += maxLevelOverlaySize;
+
 		const bool momExists = LoadOverlayInfo(ovInfo, false, 155);
 
 		if (momExists)
 		{
 			const unsigned momOverlaySize = ovInfo.loadSize + ovInfo.bssSize;
 
-			extraHeapStorage += momOverlaySize;
+			memRanges[2].start += momOverlaySize;
 		}
-	}
-};
 
-class MultiHeap : public ExpandingHeap, std::conditional_t<MOM_OVERLAY_MAY_EXIST, BaseWithMOM, BaseWithoutMOM>
+		for (const MemoryRange& memRange : memRanges)
+			memRange.ConstuctHeap();
+	}
+
+	constexpr ExtraHeapIterator begin()
+	{
+		return {memRanges.begin()};
+	}
+
+	constexpr ExtraHeapIterator end()
+	{
+		return {memRanges.end()};
+	}
+}
+constinit extraHeaps;
+
+class MultiHeap : public ExpandingHeap
 {
-	static constexpr char* extraHeapEnd = std::end(UNUSED_RAM_REGION);
-
-	static ExpandingHeap& GetExtraHeap()
-	{
-		return *std::launder(reinterpret_cast<ExpandingHeap*>(extraHeapStorage));
-	}
-
-	static bool IsInExtraHeap(const void* ptr)
-	{
-		static constexpr std::less<const void*> less = {};
-
-		return !less(ptr, extraHeapStorage) && less(ptr, extraHeapEnd);
-	}
-
 	ExpandingHeap& GetHeap(const void* ptr)
 	{
-		if (IsInExtraHeap(ptr))
-			return GetExtraHeap();
-		else
-			return *this;
+		for (const MemoryRange& extraHeapMemRange : extraHeaps.memRanges)
+		{
+			if (extraHeapMemRange.Contains(ptr))
+				return extraHeapMemRange.GetHeap();
+		}
+
+		return *this;
 	}
 
 public:
@@ -61,16 +149,21 @@ public:
 	virtual void VDestroy() override
 	{
 		ExpandingHeap::VDestroy();
-		GetExtraHeap().Destroy();
+
+		for (ExpandingHeap& extraHeap : extraHeaps)
+			extraHeap.Destroy();
 	}
 
 	virtual void* VAllocate(unsigned size, int align) override
 	{
-		void* res = GetExtraHeap().ExpandingHeap::VAllocate(size, align);
+		for (ExpandingHeap& extraHeap : extraHeaps)
+		{
+			void* res = extraHeap.ExpandingHeap::VAllocate(size, align);
 
-		if (!res) res = ExpandingHeap::VAllocate(size, align);
+			if (res) return res;
+		}
 
-		return res;
+		return ExpandingHeap::VAllocate(size, align);
 	}
 
 	virtual bool VDeallocate(void* ptr) override
@@ -81,7 +174,9 @@ public:
 	virtual void VDeallocateAll() override
 	{
 		ExpandingHeap::VDeallocateAll();
-		GetExtraHeap().ExpandingHeap::VDeallocateAll();
+
+		for (ExpandingHeap& extraHeap : extraHeaps)
+			extraHeap.ExpandingHeap::VDeallocateAll();
 	}
 
 	virtual unsigned VReallocate(void* ptr, unsigned newSize) override
@@ -96,45 +191,53 @@ public:
 
 	virtual unsigned VMaxAllocationUnitSize() override
 	{
-		const unsigned size1 = ExpandingHeap::VMaxAllocationUnitSize();
-		const unsigned size2 = GetExtraHeap().ExpandingHeap::VMaxAllocationUnitSize();
+		unsigned maxSize = ExpandingHeap::VMaxAllocationUnitSize();
 
-		return std::max(size1, size2);
+		for (ExpandingHeap& extraHeap : extraHeaps)
+		{
+			const unsigned newSize = extraHeap.ExpandingHeap::VMaxAllocationUnitSize();
+
+			if (maxSize < newSize)
+				maxSize = newSize;
+		}
+
+		return maxSize;
 	}
 
 	virtual unsigned VMaxAllocatableSize() override
 	{
-		const unsigned size1 = ExpandingHeap::VMaxAllocatableSize();
-		const unsigned size2 = GetExtraHeap().ExpandingHeap::VMaxAllocatableSize();
+		unsigned maxSize = ExpandingHeap::VMaxAllocatableSize();
 
-		return std::max(size1, size2);
+		for (ExpandingHeap& extraHeap : extraHeaps)
+		{
+			const unsigned newSize = extraHeap.ExpandingHeap::VMaxAllocatableSize();
+
+			if (maxSize < newSize)
+				maxSize = newSize;
+		}
+
+		return maxSize;
 	}
 
 	virtual unsigned VMemoryLeft() override
 	{
-		return ExpandingHeap::VMemoryLeft() + GetExtraHeap().ExpandingHeap::VMemoryLeft();
+		unsigned res = ExpandingHeap::VMemoryLeft();
+
+		for (ExpandingHeap& extraHeap : extraHeaps)
+			res += extraHeap.ExpandingHeap::VMemoryLeft();
+
+		return res;
 	}
 };
 
+[[gnu::flatten]]
 MultiHeap::MultiHeap(void* start, unsigned size, Heap* root, ExpandingHeapAllocator* allocator):
 	ExpandingHeap(start, size, root, allocator)
 {
-	char* const extraHeapStart = extraHeapStorage + sizeof(ExpandingHeap);
-	const unsigned extraHeapSize = extraHeapEnd - extraHeapStart;
-
-	auto* extraHeapAllocator = Heap::CreateExpandingHeapAllocator(extraHeapStart, extraHeapSize, 3);
-
-	if (!extraHeapAllocator) Crash();
-
-	new (extraHeapStorage) ExpandingHeap(extraHeapStart, extraHeapSize, nullptr, extraHeapAllocator);
+	extraHeaps.Init();
 }
 
 static_assert(sizeof(MultiHeap) == sizeof(ExpandingHeap));
 
-#if MOM_OVERLAY_MAY_EXIST
-
 asm("nsub_0201a0d8 = 0x0201a0dc");
-
-#endif
-
 asm("repl_0203c948 = _ZN9MultiHeapC1EPvjP4HeapP22ExpandingHeapAllocator");
